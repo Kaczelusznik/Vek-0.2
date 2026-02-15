7a139056954fff7402a3f9c1f572631db01d5fa0
Kaczelusznik <daniel.abramek@outlook.com>
2026-02-15 02:49:50 +0100

    chore: add railway debug logs

---
+++ src/index.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+const { Client, GatewayIntentBits, Collection } = require("discord.js");
+
+const ready = require("./events/ready");
+const interactionCreate = require("./events/interactionCreate");
+const ping = require("./commands/ping");
+
+process.on("unhandledRejection", (err) => {
+  console.error("unhandledRejection:", err);
+});
+
+process.on("uncaughtException", (err) => {
+  console.error("uncaughtException:", err);
+});
+
+console.log("Booting bot...");
+console.log("Node:", process.version);
+console.log("Has DISCORD_TOKEN:", Boolean(process.env.DISCORD_TOKEN));
+console.log("Token length:", process.env.DISCORD_TOKEN ? process.env.DISCORD_TOKEN.length : 0);
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds],
+});
+
+client.on("error", (err) => console.error("client error:", err));
+client.on("warn", (msg) => console.warn("client warn:", msg));
+
+client.commands = new Collection();
+client.commands.set(ping.data.name, ping);
+
+client.once("clientReady", () => ready(client));
+client.on("interactionCreate", (i) => interactionCreate(client, i));
+
+client.login(process.env.DISCORD_TOKEN).catch((err) => {
+  console.error("LOGIN FAILED:", err);
+  process.exit(1);
+});
