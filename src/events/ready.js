698cfe412c34882d456da3d3b7df15a2badb0f52
Kaczelusznik <daniel.abramek@outlook.com>
2026-02-15 15:12:03 +0100

    lol

--- src/events/ready.js
+++ src/events/ready.js
@@ -2,5 +2,15 @@ module.exports = (client) => {
   console.log(`Zalogowano jako ${client.user.tag}`);
   console.log("LOGGED AS:", client.user.tag);
   console.log("APPLICATION ID:", client.application.id);
+  console.log(`Zalogowano jako ${client.user.tag}`);
 
+  client.user.setPresence({
+    activities: [
+      {
+        name: "Składanie nezeckiego taboretu",
+        type: 1, // Playing
+      },
+    ],
+    status: "online",
+  });
 };
